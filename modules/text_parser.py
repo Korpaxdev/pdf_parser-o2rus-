c60abfa0bc7dd78d1cde9b148c1b03f29d98632a
korpaxdev <korpaxdev@gmail.com>
2024-02-17 17:51:59 +0300

    add text_parser

--- modules/text_parser.py
+++ modules/text_parser.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 from utils.constants import TemporaryFiles
@@ -21,45 +22,49 @@ class TextParser:
             block_position_start = element.start()
             block_position_end = blocks[index + 1].start() if index + 1 < (len(blocks)) else None
             block_text = self._text[block_position_start:block_position_end]
-            data_length = self._find_value(Patterns.BLOCK_DATA_LENGTH, block_text, "length")
-            pgn = self._find_value(Patterns.BLOCK_DATA_PGN, block_text, "pgn")
-            cat_id = self._find_value(Patterns.BLOCK_DATA_ID, block_text, "id")
+            data_length = self._find_value(Patterns.BLOCK_DATA_LENGTH, block_text)
+            pgn = self._find_value(Patterns.BLOCK_DATA_PGN, block_text)
+            cat_id = self._find_value(Patterns.BLOCK_DATA_ID, block_text)
             data["title"] = element.group(0)
             data["data_length"] = data_length
             data["pgn"] = pgn
             data["cat_id"] = cat_id
             data["params"] = self._find_params(block_text)
             data_list.append(data)
+        with open("./test.json", "w") as f:
+            json.dump(data_list, f, indent=4)
 
     def _find_params(self, block_text: str):
-        params = self._find_value(Patterns.PARAMS_BLOCK_START, block_text)
-        if not params:
+        params_table_head = self._find_value(Patterns.PARAMS_TABLE_HEAD, block_text)
+        if not params_table_head:
             return []
-        params_block_start = params.start()
-        params_block_text = block_text[params_block_start:]
+        params_table_start = params_table_head.start()
+        params_table_text = block_text[params_table_start:]
         params_data = []
-        for param in self._find_iterable_block(Patterns.PARAMS, params_block_text):
-            param_data = {"name": param.group("name"), "length": param.group("length"), "spn": param.group("spn")}
-            param_data.update(self._find_detail_param_info(param.group("name"), param.group("paragraph")))
+        for param in self._find_iterable_block(Patterns.PARAMS, params_table_text):
+            params_info = self._find_detail_param_info(param.group("name"), param.group("paragraph"))
+            if not params_info:
+                continue
+            param_data = {"name": param.group("name"), **params_info}
             params_data.append(param_data)
         return params_data
 
     def _find_detail_param_info(self, param_name: str, param_paragraph: str):
-        param_detail_info = {"slot_scaling": None, "slot_range": None}
-        param_info_pattern = Patterns.PARAM_BLOCK_PATTERN.format(
+        param_info_pattern = Patterns.PARAM_BLOCK.format(
             name=re.escape(param_name), paragraph=re.escape(param_paragraph)
         )
         param_info_block = self._find_value(param_info_pattern, self._text)
         if not param_info_block:
-            return param_detail_info
+            return
+        param_detail_info = dict()
         param_info_block_start = param_info_block.start()
         param_info_block_text = self._text[param_info_block_start:]
+        param_detail_info["length"] = self._find_value(Patterns.PARAM_DETAIL_LENGTH, param_info_block_text)
         param_detail_info["slot_scaling"] = self._find_value(
-            Patterns.PARAM_DETAIL_SLOT_SCALING_PATTERN, param_info_block_text, "slot_scaling"
+            Patterns.PARAM_DETAIL_SLOT_SCALING, param_info_block_text
         ).strip()
-        param_detail_info["slot_range"] = self._find_value(
-            Patterns.PARAM_DETAIL_SLOT_RANGE, param_info_block_text, "slot_range"
-        )
+        param_detail_info["slot_range"] = self._find_value(Patterns.PARAM_DETAIL_SLOT_RANGE, param_info_block_text)
+        param_detail_info["spn"] = self._find_value(Patterns.PARAM_DETAIL_SPN, param_info_block_text)
         return param_detail_info
 
     @staticmethod
@@ -67,8 +72,10 @@ class TextParser:
         return list(re.finditer(pattern, text, re.M))
 
     @staticmethod
-    def _find_value(pattern: str, text: str, group_name: str = None):
+    def _find_value(pattern: str, text: str, group_name: str = ""):
         match = re.search(pattern, text, re.M)
         if not match:
             return None
-        return match.group(group_name) if group_name else match
+        if group_name:
+            return match.group(group_name)
+        return match.group(1) if match.groups() else match
